c8c27f557f6b953938ef446fcb8168b3ab4a1b5d
Roman Zhydyk <romanzhydyk41@gmail.com>
2019-08-16 14:53:51 +0300

    Add task 5

--- FL11_HW15/js/hamburger.js
+++ FL11_HW15/js/hamburger.js
@@ -1,19 +1,42 @@
 function Hamburger(type, calories){
     let _calories = calories;
     let cheeseCalories = 120;
-    let cheese = false;
+    let tomatoCalories = 20;
+    let secretCalories = 100;
+    let cheese = 0;
+    let tomato = 0;
+    let secretIngredient = 0;
 
+    this.type = type;
     this.getCalories = () => _calories;
     this.setCalories = (caloriesValue) => _calories = caloriesValue;
-    this.addCheese = () =>{
-        if(!cheese){
+    this.addCheese = () => {
+        if(cheese < 1){
              _calories += cheeseCalories;
-             cheese = true;
+             cheese++;
         }else{
             console.log('Sorry, you can add cheese only once.');
         }
     }
-    this.type = type;
+    this.addTomato = () => {
+        if(tomato <= 1){
+            _calories += tomatoCalories;
+            tomato++;
+        }else{
+            console.log('Sorry, you can add tomato only twice.');
+        }
+    }
+    this.addSecretIngredient = () => {
+        if(cheese !== 0 || tomato !== 0){
+            console.log('Sorry, you can add secret ingredient only once before ingredient');
+        }else if(secretIngredient < 1){
+            _calories += secretCalories;
+            secretIngredient++;
+        }else{
+            console.log('Sorry, you can add secret ingredient only once.');
+        }
+    }    
+    
     
 }
 
@@ -21,6 +44,15 @@ const myHamburger = new Hamburger('classic', 600);
 // console.log(myHamburger.getCalories());
 // myHamburger.setCalories(700);
 // console.log(myHamburger.getCalories());
-myHamburger.addCheese();
+// myHamburger.addCheese();
+// console.log(myHamburger.getCalories());
+// myHamburger.addCheese();
+// myHamburger.addTomato();
+// console.log(myHamburger.getCalories());
+myHamburger.addTomato();
+// console.log(myHamburger.getCalories());
+// myHamburger.addTomato();
 console.log(myHamburger.getCalories());
-myHamburger.addCheese();
+console.log(myHamburger.getCalories());
+myHamburger.addSecretIngredient();
+console.log(myHamburger.getCalories());
