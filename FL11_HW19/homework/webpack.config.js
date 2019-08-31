d46c879c573ce868baf8e39c3c0011283c3a5807
Roman Zhydyk <romanzhydyk41@gmail.com>
2019-08-31 20:19:55 +0300

    Add some plugins

--- FL11_HW19/homework/webpack.config.js
+++ FL11_HW19/homework/webpack.config.js
@@ -2,10 +2,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.export = {
+module.exports = {
     entry: './src/js/index.js',
 
-    output:{
+    output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: 'js/bundle.js'
     },
+    module: {
+        rules: [
+          {
+            test: /\.scss$/i,
+            loader: ['style-loader', 'css-loader', 'sass-loader'],
+          },
+          {
+            test: /\.(png|jpe?g|gif)$/i,
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/'
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+            enforce: 'pre'
+            }
+          },
+          {
+            test: /\.html$/,
+            loader: ['html-loader']
+          }
+        ]
+      },
+    
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html'
+        })
+      ]
+}
