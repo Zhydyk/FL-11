a00647895b3a1df52cb6d49831de768dae8089a6
Roman Zhydyk <romanzhydyk41@gmail.com>
2019-09-07 14:53:07 +0300

    Add structure folder

---
+++ FL11_HW21_Angular_Part1/myNews/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { AppComponent } from './app.component';
+import { CreateNewsComponent } from './create-news/create-news.component';
+import { NewsLineDetailsComponent } from './news-line-details/news-line-details.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { NewsComponent } from './news/news.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'create-news', component: CreateNewsComponent},
+  {path: 'news-details', component: NewsLineDetailsComponent},
+  {path: '**', component: NotFoundComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ FL11_HW21_Angular_Part1/myNews/src/app/news-line-details/news-line-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-line-details',
+  templateUrl: './news-line-details.component.html',
+  styleUrls: ['./news-line-details.component.scss']
+})
+export class NewsLineDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
